eb743851269b32ab995dcb53d6053f4c40f9a860
Joe Lekie <joe.lekie@gmail.com>
2015-05-22 14:11:40 +0000

    Added check for if initialization promise is rejected when creating and intializing synchronously

--- src/lib/core.js
+++ src/lib/core.js
@@ -89,6 +89,7 @@ module.exports = function (Promise) {
         'createAndInitialize': function () {
             var obj = this.create.apply(this, arguments);
             if (!obj.isInitialized()) { throw new Error('Synchronous initialization expected object to be immediately initialized') }
+            if (obj.initializedPromise.isRejected()) { throw obj.initializedPromise.reason() }
             
             return obj;
         },
